337e5f83b9c5118cbb8484f621fc08ff3dd9f80f
Julien Bertram <bertramjulien@gmail.com>
2022-12-15 17:38:33 +0100

    model added & routes done

---
+++ db/migrate/20221215163533_create_coordinates.rb
@@ -0,0 +1,10 @@
+class CreateCoordinates < ActiveRecord::Migration[7.0]
+  def change
+    create_table :coordinates do |t|
+      t.float :latitude
+      t.float :longitude
+
+      t.timestamps
+    end
+  end
+end
